8b5fe9f1c6bbb8707eacff3523adcf5eec2f7a6f
Sofie <sov.002@edu.zealand.dk>
2026-08-26 13:57:23 +0200

    first commit

---
+++ components/Navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <a href=""> <li>Dine Sider</li></a>
+        <a href=""> <li>Opret side</li></a>
+        <a href=""> <li>Galleri</li></a>
+        <a href=""> <li>Settings</li></a>
+
+    </ul>
+</nav>
---
+++ include/database.php
@@ -0,0 +1,6 @@
+$connect = mysqli_connect('localhost', 'cms-becktIt', 'password, 'cms-becktIt', 3307);
+
+
+if (mysqli_connect_errno()) {
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+} 
