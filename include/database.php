456dc7cc4f7595e8d04767e5eada6cb62a011c1d
Dit Navn <din@mail.dk>
2026-08-27 11:43:39 +0200

    Mappestrukturen sat op

--- include/database.php
+++ include/database.php
@@ -1,4 +1,4 @@
-$connection = mysqli_connect('localhost', 'cms-becktit', 'password, 'cms-becktit');
+$connection = mysqli_connect('localhost', 'cms-becktit', 'password', 'cms-becktit');
 
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
