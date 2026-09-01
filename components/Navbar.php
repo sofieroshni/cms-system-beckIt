e204c6b85f88209baea7fdeba3d317b84af24529
Sofie <sov.002@edu.zealand.dk>
2026-09-01 12:44:48 +0200

    navbar animation done

--- components/Navbar.php
+++ components/Navbar.php
@@ -3,12 +3,24 @@
         <h1 class="adminpanelh1">Adminpanel</h1> </div>
     <ul class="links">
         <a href="index.php" > <li class="active">Dine Sider</li></a>
-        <a href="create-page.php"> <li>Opret side</li></a>
+        <a href="#" onClick="toggleSlide()"> <li>Opret side</li></a>
         <a href="Gallery.php"> <li>Galleri</li></a>
         <a href=""> <li>Settings</li></a>
 
     </ul>
+    <div class="slider" id="slider">
+    <li>Skabeloner
+            <ul>
+                <li>Forside</li>
+                 <li>Bronzeudstillinger</li>
+
+            </ul>
+    </li> 
+      
+    <li class="blank">Blank Side</li>
+</div> 
 </nav>
+
 <style>
 :root {
     --blue: #5271AC;
@@ -89,5 +101,75 @@ a{
     font-size: 32px;
     color: white;
 }
+/* //HVID DIV BOKS */
+
+.nav > .slider{
+ background-color:white;
+ font-family: 'Jost', sans-serif;
+list-style-type:none;
+width:230px;
+height: 330px;
+position:absolute;
+z-index:-1;
+left:230px;
+top:100px;
+color:white;
+padding:8px;
+
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+
+
+/* //
+Animationen 
+*/
+transform: translateX(-100%);
+transition: transform 0.3s ease;
+
+/* //den er som udgangspunk unsynligt */
+opacity:0;
+
+
+}
+
+.nav > .slider.active{
+    transform: translateX(0);
+    opacity:1;
+}
+/* //de to lyseblå knapper*/
+.nav > .slider >li{
+    background-color: var(--blue);
+    font-weight:700;
+    width:200px;
+    background-color:green;
+     margin-top:8px;
+     padding-top:8px;
+    border-radius:3px;
+
+
+}
+/* //øvrige knapper */
+.nav >.slider>li>ul>li{
+    background-color:#354B73;
+    font-weight:100;
+    padding:8px;
+    background-color:red;
+
+}
+/* //øvrige knapper (sidste) */
+
+.nav >.slider>li:last-child{
+    font-weight:100;
+    margin-top:25px;
+}
+
 
 </style>
+<script>
+function toggleSlide(){
+    document.getElementById('slider').classList.toggle('active');
+}
+
+</script>
