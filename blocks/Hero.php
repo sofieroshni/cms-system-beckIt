2c709e27a3a7894c38deb6654f7966ab19a441d8
Sofie <sov.002@edu.zealand.dk>
2026-09-02 13:01:25 +0200

    hero-styled

--- blocks/Hero.php
+++ blocks/Hero.php
@@ -21,12 +21,77 @@ class HeroBlock implements BlockInterface {
 
         return "
             <section class='hero' style=\"background-image:url('{$bg}')\">
-                <div class='hero-box'>
-                    <h1>{$title}</h1>
+            <div class='hero-title'>      
+                  <h1>{$title}</h1>
+                    </div>      
+                    <div class='hero-info'> 
                     <span>{$address}</span>
                     <span>{$phone}</span>
-                </div>
+                    </div>
+                   
             </section>
-        ";
+     <style>
+     
+        .hero {
+            
+            font-family: 'Jost', sans-serif;
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+
+            background-size: cover;
+            background-position: center;
+            height: 58px;
+                
+           }
+           h1{color:white;
+            font-size:40px;
+            font-weigt:extra-bold;
+font-family: 'Jost', sans-serif;
+letter-spacing: 5px;
+}
+
+        .hero > .hero-title{
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            text-align:center;
+            width:458px;
+            background-color:#213377;
+            margin-bottom:8px;
+            }
+
+        .hero > .hero-info{   
+            display:flex;
+            text-align:center;
+            justify-content:space-between;
+            width:458px;
+            padding:0px;
+            text-align:center;
+
+
+            }
+
+            .hero > .hero-info >span{
+            display:flex;
+            // background-color:red;
+            color:white;
+            font-size:16px;
+            font-weight:medium;
+            width:225px;
+            border-radius:3px;
+             background-color:#213377;
+            text-align:center;
+            align-items:center;
+            justify-content:center;
     }
+
+        </style>
+        ";
+ 
+         
+    }
+  
 }
