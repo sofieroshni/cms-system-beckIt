06fffd4b5f9f322307f90e63914a77104215bc1e
Sofie <sov.002@edu.zealand.dk>
2026-09-07 13:54:16 +0200

    bridgetabel oprettet

--- blocks/Tabel.php
+++ blocks/Tabel.php
@@ -12,7 +12,26 @@ require_once __DIR__ . '/BlockInterface.php';
         }
         public static function render (array $data): string {
             $contentTitle = htmlspecialchars($data['contentTitle'] ?? 'hej');
+
+              return "
+        <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr> 
+  <style>
+  </style>;
+  ";
         }
-       
-    }
+        
+      
+        }
+        
+    
  
