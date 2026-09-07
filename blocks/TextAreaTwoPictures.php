9e3255a386ddd4eea17694c974e75d3e9ae043b0
Sofie <sov.002@edu.zealand.dk>
2026-09-07 11:33:39 +0200

    textAreaTwoPictures.php

--- blocks/TextAreaTwoPictures.php
+++ blocks/TextAreaTwoPictures.php
@@ -1,42 +1,62 @@
 <?php
+
 require_once __DIR__ . '/BlockInterface.php';
 
-class TextAreaTwoPicturesBlock implements BlockInterface {
-    public static function getSchema(): array {
+class TextAreaTwoPicturesBlock implements BlockInterface
+{
+    public static function getSchema(): array
+    {
         return [
-            'contentTitle'=> ['type' => 'text', 'label' => 'Titel'],
-            'content' => ['type' => 'richtext', 'label' => 'Tekst'],
-            'TextsArea' => ['type'=> 'richtext' , 'label' => 'Tekst grå'],
-            'TextArea3' => ['type' => 'text', 'label' => 'Titel grå']
+            'contentTitle' => [
+                'type' => 'text',
+                'label' => 'Titel'
+            ],
+            'content' => [
+                'type' => 'richtext',
+                'label' => 'Tekst'
+            ],
         ];
     }
 
-    public static function render(array $data): string {        
-        $title = htmlspecialchars($data['contentTitle'] ?: 'indsæt under-overskrift');
-        $grayText = htmlspecialchars($data['content'] ?: 'indsæt tekst');
-        $textArea = htmlspecialchars($data['TextsArea'] ?? 'tekst felt tekst felt tekst felt tekst felt tekst felt tekst felt tekst felt tekst felt tekst felt 
-tekst felt tekst felt tekst felt tekst felt tekst felt tekst felt tekst felt 
-tekst felt tekst felt tekst felt tekst felt tekst felt ');
-        $textArea3 = htmlspecialchars($data['TextArea3'] ?? 'H3' );
+    public static function render(array $data): string
+    {
+        $title = htmlspecialchars(
+            $data['contentTitle'] ?? 'Indsæt under-overskrift'
+        );
+
+        $whitetext = htmlspecialchars(
+            $data['content'] ?: 'Indsæt tekst, Indsæt tekst,Indsæt tekst,Indsæt tekst,Indsæt tekst,Indsæt tekst'
+        );
+
         return "
-        <section>
-        
-        </section>
+            <section>
+                <div class='mudbackground'>
+                    <div class='column'>
+                    {$title}
+                    {$whitetext}
+                    </div>
+                     <div class='column'></div>
 
+                </div>
+            </section>
 
-         <style>
-    section{
-    background-color:#686666;
-    width:100%;
-    height:400px;
-    display:flex;
+            <style>
+                .mudbackground {
+                    background-color: #686666;
+                    width: 100%;
+                    display: flex;
+                    justify-content:center;
+                    flex-direction:row;
+                    font-family: 'Jost', sans-serif;
+                }
+                    .column{
+                    background-color:red;
+                    width:50%;
+                    display: flex;
+                    flex-direction: column;
 
-
-    font-family: 'Jost', sans-serif;
-
+                    }
+            </style>
+        ";
     }
-    
-</style>";
-    }
-  
 }
